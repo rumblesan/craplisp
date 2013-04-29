aa5fa12d103240f67a59821880e808efcf91fafb
Guy John <guy@rumblesan.com>
2013-04-29 14:18:05 +0100

    Adding runner and test program

---
+++ craplisp.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+from tokeniser import tokeniser
+from parser import parser
+from interpreter import interpreter
+
+from sys import argv
+
+
+def main():
+    inputfile = argv[1]
+
+    with open(inputfile) as fp:
+        input_program = fp.read()
+
+    t = tokeniser()
+    p = parser()
+    i = interpreter()
+
+    t.load(input_program)
+    t.tokenise()
+
+    p.setup(t.output)
+    p.parse()
+
+    i.setup(p.output)
+    result = i.run()
+
+    print(result)
+
+
+if __name__ == '__main__':
+    main()
---
+++ scripts/maths.cl
@@ -0,0 +1,9 @@
+
+(define x 5)
+
+(define y 7)
+
+(define z 9)
+
+(+ (* 3 y 5) (* z y) (/ x 10))
+
