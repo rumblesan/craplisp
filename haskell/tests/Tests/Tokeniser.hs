bd3403f47631bf7d1138bc519f5d0fef9716d082
Guy John <guy@rumblesan.com>
2013-05-05 17:02:18 +0100

    Updating tests for Tokeniser

--- haskell/tests/Tests/Tokeniser.hs
+++ haskell/tests/Tests/Tokeniser.hs
@@ -1,7 +1,7 @@
 
 module Tests.Tokeniser ( testTokeniser ) where
 
-import Tokeniser ( tokenise, parse_number, parse_symbol, parse_string )
+import Tokeniser
 import Test.Framework (Test, testGroup)
 import Test.Framework.Providers.HUnit (testCase)
 import Test.HUnit hiding (Test)
@@ -9,40 +9,40 @@ import Test.HUnit hiding (Test)
 testParseEmptyNumber :: Test
 testParseEmptyNumber = testCase "empty string number" $ assertEqual
     "Should get empty string and list with a list from empty input"
-    ("", [[]])
+    ("", [])
     ( parse_number "" "" [] )
 
 testParseNumber :: Test
 testParseNumber = testCase "parse number" $ assertEqual
     "Should get empty string and list with a number string in"
-    ("", ["1234"])
+    ("", [Number 1234])
     ( parse_number "" "1234" [] )
 
 testParseNumberFollowedByChars :: Test
 testParseNumberFollowedByChars = testCase "parse number in string" $ assertEqual
     "Should get only initial number chars and list with a number string in"
-    ("abcdef456", ["1234"])
+    ("abcdef456", [Number 1234])
     ( parse_number "" "1234abcdef456" [] )
 
 
 testParseSymbol :: Test
 testParseSymbol = testCase "parse symbol" $ assertEqual
     "Should get string of spaces and list with a symbol string in"
-    (" ", ["++"])
+    (" ", [Symbol "++"])
     ( parse_symbol "" "++ " [] )
 
 
 testParseString :: Test
 testParseString = testCase "parse string" $ assertEqual
     "Should get string of spaces and list with a string in"
-    ("  ", ["string"])
+    ("  ", [Str "string"])
     ( parse_string "" "\"string\"  " [] )
 
 
 testTokeniseProgram :: Test
 testTokeniseProgram = testCase "tokenise program" $ assertEqual
     "Should get a list of tokens"
-    ["(", "+", "34673", "(", "double", "903245", ")", ")"]
+    [OpenParen, Symbol "+", Number 34673, OpenParen, Symbol "double", Number 903245, CloseParen, CloseParen]
     ( tokenise " ( + 34673 ( double 903245 ) )  " )
 
 
