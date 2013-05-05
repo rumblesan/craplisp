45a5e193964eec83d812b0a96d83dc1d04aeb223
Guy John <guy@rumblesan.com>
2013-05-05 14:17:40 +0100

    First go at the parser... I think it works!

---
+++ haskell/src/CrapParser.hs
@@ -0,0 +1,30 @@
+
+module CrapParser (parse, parseExpr, subExpr) where
+
+
+data Token = Token String deriving (Show, Eq)
+
+data Expression = Node Token
+                | List [Expression] deriving (Show, Eq)
+
+
+parse :: [Token] -> [Expression]
+parse [] = []
+parse tokens = fst $ parseExpr ([], tokens)
+
+
+parseExpr :: ([Expression], [Token]) -> ([Expression], [Token])
+parseExpr (current, []) = (current, [])
+parseExpr (current, (i:is))
+    | isOpen i = subExpr (current, is)
+    | isClose i = (current, is)
+    | otherwise = parseExpr (current ++ [Node i], is)
+    where isOpen t = t == Token "("
+          isClose t = t == Token ")"
+
+subExpr :: ([Expression], [Token]) -> ([Expression], [Token])
+subExpr (current, []) = (current, [])
+subExpr (output, tokens) =
+    let (newExpr, remaining) = parseExpr ([], tokens)
+    in parseExpr (output ++ [List newExpr], remaining)
+
